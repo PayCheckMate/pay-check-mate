da6b299a87942b592b9d5e826292f7810b809f04
ratulhasan <ratuljh@gmail.com>
2023-05-24 18:40:42 +0600

    Fix nonce name

--- includes/Requests/DepartmentFormRequest.php
+++ includes/Requests/DepartmentFormRequest.php
@@ -6,6 +6,8 @@ use PayCheckMate\Abstracts\FormRequest;
 
 class DepartmentFormRequest extends FormRequest {
 
-	protected string $nonce = 'pay-check-mate-noncess';
+	protected string $nonce = 'pay_check_mate_nonce';
+
+	// Have to create a rule that will validate $request in next.
 
 }
