e29c0d4ebfca0bfed69b520747686b9327372c34
ratulhasan <ratuljh@gmail.com>
2023-05-28 23:00:07 +0600

    ✨Place Chevron icon in the right end

--- src/Components/NavbarLink.tsx
+++ src/Components/NavbarLink.tsx
@@ -55,15 +55,18 @@ export const NavbarLink = ({ navigation }: NavbarLinkProps) => {
                                                 aria-hidden="true"
                                             />
                                         )}
-                                        {item.title}
-                                        <ChevronDownIcon
-                                            className={classNames(
-                                                "ml-2 h-5 w-5 text-gray-400 text-right group-hover:text-gray-300 transition ease-in-out duration-150",
-                                                expandedLinks.includes(index) ? "transform rotate-180" : ""
-                                            )}
-                                            aria-hidden="true"
-                                        />
+                                        <div className="flex items-center justify-between w-full">
+                                            <div>{item.title}</div>
+                                            <ChevronDownIcon
+                                                className={classNames(
+                                                    "ml-2 h-5 w-5 text-gray-400 text-right group-hover:text-gray-300 transition ease-in-out duration-150",
+                                                    expandedLinks.includes(index) ? "transform rotate-180" : ""
+                                                )}
+                                                aria-hidden="true"
+                                            />
+                                        </div>
                                     </div>
+
                                     {expandedLinks.includes(index) && (
                                         <ul role="list" className="pl-6 py-2 space-y-2">
                                             <NavbarLink navigation={item.children} />
