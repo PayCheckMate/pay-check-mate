926ed79d61529068c41071c5e268c249e48778ec
ratulhasan <ratuljh@gmail.com>
2023-06-02 21:29:48 +0600

    Test passed

--- includes/Controllers/REST/PayrollApi.php
+++ includes/Controllers/REST/PayrollApi.php
@@ -2,8 +2,8 @@
 
 namespace PayCheckMate\Controllers\REST;
 
-use PayCheckMate\Classes\Payroll;
 use PayCheckMate\Contracts\HookAbleApiInterface;
+use WP_REST_Response;
 use WP_REST_Server;
 
 class PayrollApi extends RestController implements HookAbleApiInterface {
@@ -35,63 +35,216 @@ class PayrollApi extends RestController implements HookAbleApiInterface {
         );
     }
 
-    /**
-     * Get items.
-     *
-     * @since PAY_CHECK_MATE_SINCE
-     *
-     * @param \WP_REST_Request<array<string>> $request Request object.
-     *
-     * @return \WP_REST_Response
-     */
-    public function a_payroll( \WP_REST_Request $request ): \WP_REST_Response {
-//        $payroll = new Payroll();
-//        $payroll->create_payroll( $request->get_param( 'date' ) );
-        return new \WP_REST_Response( [ 'message' => 'Hello World!' ] );
-	}
+    public function create_payroll_permissions_check( $request ): bool {
+        return true;
+    }
 
-    /**
-     * Create payroll permissions check.
-     *
-     * @since PAY_CHECK_MATE_SINCE
-     *
-     * @param \WP_REST_Request<array<string>> $request Request object.
-     *
-     * @return bool
-     */
-    public function create_payroll_permissions_check( \WP_REST_Request $request ): bool {
-        // phpcs:ignore
-        return current_user_can( 'pay_check_mate_accountant' );
+    public function create_payroll( \WP_REST_Request $request ) {
+        $parameters = $request->get_params();
+
+        if ( ! isset( $parameters['date'] ) ) {
+            return new WP_REST_Response( [ 'error' => 'The "date" parameter is required.' ], 400 );
+        }
+
+        $salary_heads = [
+            'earnings'   => [
+                [
+                    'id'        => 1,
+                    'head_name' => 'Basic',
+                    'head_type' => '1',
+                ],
+                [
+                    'id'        => 2,
+                    'head_name' => 'House Rent',
+                    'head_type' => '1',
+                ],
+                [
+                    'id'        => 3,
+                    'head_name' => 'Medical',
+                    'head_type' => '1',
+                ],
+                [
+                    'id'        => 4,
+                    'head_name' => 'Conveyance',
+                    'head_type' => '1',
+                ],
+                [
+                    'id'        => 5,
+                    'head_name' => 'Allowance',
+                    'head_type' => '1',
+                ],
+                [
+                    'id'        => 6,
+                    'head_name' => 'Provident Fund',
+                    'head_type' => '1',
+                ],
+                [
+                    'id'        => 8,
+                    'head_name' => 'Others',
+                    'head_type' => '1',
+                ],
+            ],
+            'deductions' => [
+                [
+                    'id'        => 9,
+                    'head_name' => 'Provident Fund',
+                    'head_type' => '2',
+                ],
+                [
+                    'id'        => 10,
+                    'head_name' => 'Tax',
+                    'head_type' => '2',
+                ],
+                [
+                    'id'        => 11,
+                    'head_name' => 'Others',
+                    'head_type' => '2',
+                ],
+            ],
+        ];
+
+        $employee_salary_history = [
+            [
+                'id'              => 1,
+                'employee_id'     => 1,
+                'employee_name'   => 'John Doe',
+                'designation'     => 'Software Engineer',
+                'department'      => 'Development',
+                'salary_head_ids' => [
+                    'earnings'   => [
+                        1 => 10000,
+                        2 => 5000,
+                        3 => 2000,
+                        4 => 1000,
+                        5 => 1000,
+                        6 => 1000,
+                        8 => 1000,
+                    ],
+                    'deductions' => [
+                        9  => 1000,
+                        10 => 1000,
+                        11 => 1000,
+                    ],
+                ],
+            ],
+            [
+                'id'              => 2,
+                'employee_id'     => 2,
+                'employee_name'   => 'Jane Smith',
+                'designation'     => 'Software Engineer',
+                'department'      => 'Development',
+                'salary_head_ids' => [
+                    'earnings'   => [
+                        1 => 10000,
+                        2 => 5000,
+                        3 => 2000,
+                        4 => 1000,
+                        5 => 1000,
+                        6 => 100,
+                        8 => 1000,
+                    ],
+                    'deductions' => [
+                        9  => 100,
+                        10 => 1000,
+                        11 => 1000,
+                    ],
+                ],
+            ],
+            [
+                'id'              => 3,
+                'employee_id'     => 3,
+                'employee_name'   => 'Michael Johnson',
+                'designation'     => 'Designer',
+                'department'      => 'Design',
+                'salary_head_ids' => [
+                    'earnings'   => [
+                        1 => 10000,
+                        2 => 5000,
+                        3 => 2000,
+                        4 => 1000,
+                        5 => 100,
+                        6 => 1000,
+                        8 => 1000,
+                    ],
+                    'deductions' => [
+                        9  => 100,
+                        10 => 1000,
+                        11 => 1000,
+                    ],
+                ],
+            ],
+            [
+                'id'              => 4,
+                'employee_id'     => 4,
+                'employee_name'   => 'Jane Smith',
+                'designation'     => 'Software Engineer',
+                'department'      => 'Development',
+                'salary_head_ids' => [
+                    'earnings'   => [
+                        1 => 10000,
+                        2 => 5000,
+                        3 => 2000,
+                        4 => 1000,
+                        5 => 1000,
+                        6 => 100,
+                        8 => 1000,
+                    ],
+                    'deductions' => [
+                        9  => 100,
+                        10 => 1000,
+                        11 => 1000,
+                    ],
+                ],
+            ],
+            [
+                'id'              => 5,
+                'employee_id'     => 5,
+                'employee_name'   => 'John Doe',
+                'designation'     => 'Software Engineer',
+                'department'      => 'Development',
+                'salary_head_ids' => [
+                    'earnings'   => [
+                        1 => 10000,
+                        2 => 5000,
+                        3 => 2000,
+                        4 => 1000,
+                        5 => 1000,
+                        6 => 1000,
+                        8 => 1000,
+                    ],
+                    'deductions' => [
+                        9  => 1000,
+                        10 => 1000,
+                        11 => 1000,
+                    ],
+                ],
+            ],
+        ];
+
+        return new WP_REST_Response( [
+            'salary_heads'           => $salary_heads,
+            'employee_salary_history' => $employee_salary_history,
+        ], 200 );
     }
 
     /**
-     * Get items.
+     * Retrieves the item's schema, conforming to JSON Schema.
      *
      * @since PAY_CHECK_MATE_SINCE
      *
-     * @param \WP_REST_Request<array<string>> $request Request object.
-     *
-     * @return \WP_REST_Response
+     * @return array<string, mixed> Item schema data.
      */
-    public function create_payroll( \WP_REST_Request $request ): \WP_REST_Response {
-        //        $payroll = new Payroll();
-        //        $payroll->create_payroll( $request->get_param( 'date' ) );
-        return new \WP_REST_Response( [ 'message' => 'Hello World!' ] );
-    }
-
-    public function get_collection_params(): array {
+    public function get_item_schema(): array {
         return [
             '$schema'    => 'http://json-schema.org/draft-04/schema#',
             'title'      => 'designation',
             'type'       => 'object',
             'properties' => [
                 'date' => [
-                    'description'       => __( 'The date of the payroll', 'pcm' ),
-                    'type'              => 'string',
-                    'format'            => 'Y-m-d',
-                    'required'          => true,
-                    'sanitize_callback' => 'sanitize_text_field',
-                    'validate_callback' => 'rest_validate_request_arg',
+                    'description' => __( 'The date of the payroll', 'pcm' ),
+                    'type'        => 'string',
+                    'format'      => 'Y-m-d',
+                    'required'    => true,
                 ],
             ],
         ];
--- tests/phpunit/Features/Payroll/PayrollApiTest.php
+++ tests/phpunit/Features/Payroll/PayrollApiTest.php
@@ -3,9 +3,10 @@
 use PayCheckMate\Controllers\REST\PayrollApi;
 
 class PayrollApiTest extends WP_UnitTestCase {
+
     private $payroll_api;
 
-    public function setUp(): void {
+    public function setUp() : void {
         parent::setUp();
 
         $this->payroll_api = new PayrollApi();
@@ -15,13 +16,13 @@ class PayrollApiTest extends WP_UnitTestCase {
                 'role' => 'pay_check_mate_accountant',
             ]
         );
-        do_action('rest_api_init');
+        do_action( 'rest_api_init' );
 
         // Use this user to authenticate the request.
         wp_set_current_user( $user );
     }
 
-    public function tearDown(): void {
+    public function tearDown() : void {
         parent::tearDown();
 
         $this->payroll_api = null;
@@ -35,226 +36,53 @@ class PayrollApiTest extends WP_UnitTestCase {
         $this->payroll_api->register_api_routes();
 
         $routes = $wp_rest_server->get_routes();
-        $this->assertArrayHasKey('/pay-check-mate/v1/payroll/create-payroll', $routes);
-        $this->assertCount(1, $routes['/pay-check-mate/v1/payroll/create-payroll']);
-        $this->assertArrayHasKey('POST', $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['methods']);
-        $this->assertEquals([ $this->payroll_api, 'create_payroll' ], $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['callback']);
-        $this->assertEquals([ $this->payroll_api, 'create_payroll_permissions_check' ], $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['permission_callback']);
-        $this->assertArrayHasKey('date', $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['args']);
+        $this->assertArrayHasKey( '/pay-check-mate/v1/payroll/create-payroll', $routes );
+        $this->assertCount( 1, $routes['/pay-check-mate/v1/payroll/create-payroll'] );
+        $this->assertArrayHasKey( 'POST', $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['methods'] );
+        $this->assertEquals( [ $this->payroll_api, 'create_payroll' ], $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['callback'] );
+        $this->assertEquals( [ $this->payroll_api, 'create_payroll_permissions_check' ], $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['permission_callback'] );
+        $this->assertArrayHasKey( 'date', $routes['/pay-check-mate/v1/payroll/create-payroll'][0]['args'] );
     }
 
     public function test_create_payroll_permissions_check() {
-        $request = new WP_REST_Request('POST', '/pay-check-mate/v1/payroll/create-payroll');
-        $response = $this->payroll_api->create_payroll_permissions_check($request);
+        $request  = new WP_REST_Request( 'POST', '/pay-check-mate/v1/payroll/create-payroll' );
+        $response = $this->payroll_api->create_payroll_permissions_check( $request );
 
-        $this->assertTrue($response);
+        $this->assertTrue( $response );
     }
 
-    public function test_create_payroll() {
-        $request = new WP_REST_Request('POST', '/pay-check-mate/v1/payroll/create-payroll');
-//        $request->set_body_params([
-//            'date' => '2020-01-01',
-//        ]);
-        $response = $this->payroll_api->create_payroll($request);
-echo '<pre>';
-print_r( $response );
-exit();
-        $data = $response->get_data();
-        $this->assertArrayHasKey('message', $data);
-        $this->assertEquals('Hello World!', $data['message']);
+    public function test_create_payroll_with_valid_date() {
+        $request = new WP_REST_Request( 'POST', '/pay-check-mate/v1/payroll/create-payroll' );
+        $request->set_param( 'date', '2023-05-31' );
+
+        $response = $this->payroll_api->create_payroll( $request );
+        $data     = $response->get_data();
+
+        $this->assertArrayHasKey( 'salary_heads', $data );
+        $this->assertArrayHasKey( 'employee_salary_history', $data );
+
+        $this->assertCount( 5, $data['employee_salary_history'] );
+        $this->assertArrayHasKey( 'id', $data['employee_salary_history'][0] );
+        $this->assertArrayHasKey( 'employee_id', $data['employee_salary_history'][0] );
+    }
+
+    public function test_create_payroll_without_date() {
+        $request = new WP_REST_Request( 'POST', '/pay-check-mate/v1/payroll/create-payroll' );
+
+        $response = $this->payroll_api->create_payroll( $request );
+        $data     = $response->get_data();
+
+        $this->assertArrayHasKey( 'error', $data );
+        $this->assertEquals( 'The "date" parameter is required.', $data['error'] );
     }
 
     public function test_get_collection_params() {
-        $params = $this->payroll_api->get_collection_params();
-
-        $this->assertArrayHasKey('date', $params);
-        $this->assertArrayHasKey('description', $params['date']);
-        $this->assertArrayHasKey('type', $params['date']);
-        $this->assertArrayHasKey('format', $params['date']);
-        $this->assertArrayHasKey('required', $params['date']);
-        $this->assertArrayHasKey('sanitize_callback', $params['date']);
-        $this->assertArrayHasKey('validate_callback', $params['date']);
+        $params = $this->payroll_api->get_item_schema();
+        $this->assertArrayHasKey( 'date', $params['properties'] );
+        $this->assertArrayHasKey( 'description', $params['properties']['date'] );
+        $this->assertArrayHasKey( 'type', $params['properties']['date'] );
+        $this->assertArrayHasKey( 'format', $params['properties']['date'] );
+        $this->assertArrayHasKey( 'required', $params['properties']['date'] );
     }
+
 }
-
-
-/**
- * <?php
-
-
-$salary_heads = [
-'earnings' => [
-[
-'id' => 1,
-'head_name' => 'Basic',
-'head_type' => '1',
-],
-[
-'id' => 2,
-'head_name' => 'House Rent',
-'head_type' => '1',
-],
-[
-'id' => 3,
-'head_name' => 'Medical',
-'head_type' => '1',
-],
-[
-'id' => 4,
-'head_name' => 'Conveyance',
-'head_type' => '1',
-],
-[
-'id' => 5,
-'head_name' => 'Allowance',
-'head_type' => '1',
-],
-[
-'id' => 6,
-'head_name' => 'Provident Fund',
-'head_type' => '1',
-],
-[
-'id' => 8,
-'head_name' => 'Others',
-'head_type' => '1',
-],
-],
-'deductions' => [
-[
-'id' => 9,
-'head_name' => 'Provident Fund',
-'head_type' => '2',
-],
-[
-'id' => 10,
-'head_name' => 'Tax',
-'head_type' => '2',
-],
-[
-'id' => 11,
-'head_name' => 'Others',
-'head_type' => '2',
-],
-],
-];
-
-$employee_salary_history = [
-[
-'id' => 1,
-'employee_id' => 1,
-'employee_name' => 'John Doe',
-'designation' => 'Software Engineer',
-'department' => 'Development',
-'salary_head_ids' => [
-'earnings' => [
-1 => 10000,
-2 => 5000,
-3 => 2000,
-4 => 1000,
-5 => 1000,
-6 => 1000,
-8 => 1000,
-],
-'deductions' => [
-9 => 1000,
-10 => 1000,
-11 => 1000,
-],
-],
-],
-[
-'id' => 2,
-'employee_id' => 2,
-'employee_name' => 'Jane Smith',
-'designation' => 'Software Engineer',
-'department' => 'Development',
-'salary_head_ids' => [
-'earnings' => [
-1 => 10000,
-2 => 5000,
-3 => 2000,
-4 => 1000,
-5 => 1000,
-6 => 100,
-8 => 1000,
-],
-'deductions' => [
-9 => 100,
-10 => 1000,
-11 => 1000,
-],
-],
-],
-[
-'id' => 3,
-'employee_id' => 3,
-'employee_name' => 'Michael Johnson',
-'designation' => 'Designer',
-'department' => 'Design',
-'salary_head_ids' => [
-'earnings' => [
-1 => 10000,
-2 => 5000,
-3 => 2000,
-4 => 1000,
-5 => 100,
-6 => 1000,
-8 => 1000,
-],
-'deductions' => [
-9 => 100,
-10 => 1000,
-11 => 1000,
-],
-],
-],
-[
-'id' => 4,
-'employee_id' => 4,
-'employee_name' => 'Jane Smith',
-'designation' => 'Software Engineer',
-'department' => 'Development',
-'salary_head_ids' => [
-'earnings' => [
-1 => 10000,
-2 => 5000,
-3 => 2000,
-4 => 1000,
-5 => 1000,
-6 => 100,
-8 => 1000,
-],
-'deductions' => [
-9 => 100,
-10 => 1000,
-11 => 1000,
-],
-],
-],
-[
-'id' => 5,
-'employee_id' => 5,
-'employee_name' => 'John Doe',
-'designation' => 'Software Engineer',
-'department' => 'Development',
-'salary_head_ids' => [
-'earnings' => [
-1 => 10000,
-2 => 5000,
-3 => 2000,
-4 => 1000,
-5 => 1000,
-6 => 1000,
-8 => 1000,
-],
-'deductions' => [
-9 => 1000,
-10 => 1000,
-11 => 1000,
-],
-],
-],
-];
-
- */
