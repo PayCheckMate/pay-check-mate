ae8032757040ca724209cac54119aa3f3adfd459
ratulhasan <ratuljh@gmail.com>
2023-06-21 19:53:31 +0600

    🛠️ [FIX] Phpcs problems

--- includes/Controllers/REST/DesignationApi.php
+++ includes/Controllers/REST/DesignationApi.php
@@ -28,71 +28,71 @@ class DesignationApi extends RestController implements HookAbleApiInterface {
     public function register_api_routes(): void {
         register_rest_route(
             $this->namespace, '/' . $this->rest_base, [
-            [
-                'methods'             => \WP_REST_Server::READABLE,
-                'callback'            => [ $this, 'get_items' ],
-                'args'                => $this->get_collection_params(),
-                'permission_callback' => [ $this, 'get_items_permissions_check' ],
-            ],
-            [
-                'methods'             => \WP_REST_Server::CREATABLE,
-                'callback'            => [ $this, 'create_item' ],
-                'permission_callback' => [ $this, 'create_item_permissions_check' ],
-                'args'                => $this->get_endpoint_args_for_item_schema( \WP_REST_Server::CREATABLE ),
-            ],
-            'schema' => [ $this, 'get_public_item_schema' ],
-        ],
+				[
+					'methods'             => \WP_REST_Server::READABLE,
+					'callback'            => [ $this, 'get_items' ],
+					'args'                => $this->get_collection_params(),
+					'permission_callback' => [ $this, 'get_items_permissions_check' ],
+				],
+				[
+					'methods'             => \WP_REST_Server::CREATABLE,
+					'callback'            => [ $this, 'create_item' ],
+					'permission_callback' => [ $this, 'create_item_permissions_check' ],
+					'args'                => $this->get_endpoint_args_for_item_schema( \WP_REST_Server::CREATABLE ),
+				],
+				'schema' => [ $this, 'get_public_item_schema' ],
+			],
         );
 
         register_rest_route(
             $this->namespace, '/' . $this->rest_base . '/(?P<id>[\d]+)', [
-            [
-                'methods'             => \WP_REST_Server::READABLE,
-                'callback'            => [ $this, 'get_item' ],
-                'permission_callback' => [ $this, 'get_item_permissions_check' ],
-                'args'                => [
-                    'id' => [
-                        'description' => __( 'Unique identifier for the object.', 'pcm' ),
-                        'type'        => 'integer',
-                        'required'    => true,
-                    ],
-                ],
-            ],
-            [
-                'methods'             => \WP_REST_Server::EDITABLE,
-                'callback'            => [ $this, 'update_item' ],
-                'permission_callback' => [ $this, 'update_item_permissions_check' ],
-                'args'                => [
-                    'id'     => [
-                        'description' => __( 'Unique identifier for the object.', 'pcm' ),
-                        'type'        => 'integer',
-                        'required'    => true,
-                    ],
-                    'name'   => [
-                        'description' => __( 'Designation name.', 'pcm' ),
-                        'type'        => 'string',
-                        'required'    => true,
-                    ],
-                    'status' => [
-                        'description' => __( 'Designation status.', 'pcm' ),
-                        'type'        => 'number',
-                    ],
-                ],
-            ],
-            [
-                'methods'             => \WP_REST_Server::DELETABLE,
-                'callback'            => [ $this, 'delete_item' ],
-                'permission_callback' => [ $this, 'delete_item_permissions_check' ],
-                'args'                => [
-                    'id' => [
-                        'description' => __( 'Unique identifier for the object.', 'pcm' ),
-                        'type'        => 'integer',
-                        'required'    => true,
-                    ],
-                ],
-            ],
-            'schema' => [ $this, 'get_public_item_schema' ],
-        ],
+				[
+					'methods'             => \WP_REST_Server::READABLE,
+					'callback'            => [ $this, 'get_item' ],
+					'permission_callback' => [ $this, 'get_item_permissions_check' ],
+					'args'                => [
+						'id' => [
+							'description' => __( 'Unique identifier for the object.', 'pcm' ),
+							'type'        => 'integer',
+							'required'    => true,
+						],
+					],
+				],
+				[
+					'methods'             => \WP_REST_Server::EDITABLE,
+					'callback'            => [ $this, 'update_item' ],
+					'permission_callback' => [ $this, 'update_item_permissions_check' ],
+					'args'                => [
+						'id'     => [
+							'description' => __( 'Unique identifier for the object.', 'pcm' ),
+							'type'        => 'integer',
+							'required'    => true,
+						],
+						'name'   => [
+							'description' => __( 'Designation name.', 'pcm' ),
+							'type'        => 'string',
+							'required'    => true,
+						],
+						'status' => [
+							'description' => __( 'Designation status.', 'pcm' ),
+							'type'        => 'number',
+						],
+					],
+				],
+				[
+					'methods'             => \WP_REST_Server::DELETABLE,
+					'callback'            => [ $this, 'delete_item' ],
+					'permission_callback' => [ $this, 'delete_item_permissions_check' ],
+					'args'                => [
+						'id' => [
+							'description' => __( 'Unique identifier for the object.', 'pcm' ),
+							'type'        => 'integer',
+							'required'    => true,
+						],
+					],
+				],
+				'schema' => [ $this, 'get_public_item_schema' ],
+			],
         );
     }
 
