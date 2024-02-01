16d80f7a6bcad17b5af5dcd7186c6570870d1afc
ratulhasan <ratuljh@gmail.com>
2024-02-01 19:35:19 +0600

    Update some style

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -62,7 +62,7 @@ export const Modal = ({setShowModal, children, header, description = '', width =
                                         <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                                             <Dialog.Title
                                                 as="h3"
-                                                className="text-base font-semibold leading-6 text-gray-900"
+                                                className="text-base font-semibold leading-6 text-gray-900 mb-5"
                                             >
                                                 {header}
                                             </Dialog.Title>
